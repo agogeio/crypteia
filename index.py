54c13d5656b230578213e693b2e9974e134e4302
Steven Aiello <steven.aiello@agoge.io>
2024-01-09 20:53:53 -0500

    CVE Extraction and Analysis Updates Working

--- index.py
+++ index.py
@@ -1,11 +1,17 @@
+# import nvdlib
 import os
 import pandas as pd
+import requests
+import time
 
 from os.path import exists
 from urllib.request import urlretrieve
 
 KEV_FILENAME = 'known_exploited_vulnerabilities.json'
+#! The public rate limit (without an API key) is 5 requests in a rolling 30 second window; 
+#! the rate limit with an API key is 50 requests in a rolling 30 second window. 
 API_KEY = os.environ.get("nvd_key")
+NVD_URL = "https://services.nvd.nist.gov/rest/json/cves/2.0?cveId="
 
 cve_list = []
 cve_tuple = ()
@@ -29,7 +35,7 @@ def get_KEV():
         print("CISA KEV List downloaded")
 
 
-def get_CVEs(path: str = 'VulnerabilityReport.xlsx', sheet: str = 'CVE', column: str = 'CVE'):
+def extract_CVEs(path: str = 'VulnerabilityReport.xlsx', sheet: str = 'CVE', column: str = 'CVE'):
 
     cols = [column]
     try:
@@ -51,7 +57,7 @@ def get_CVEs(path: str = 'VulnerabilityReport.xlsx', sheet: str = 'CVE', column:
                 
         cve_tuple = tuple(cve_list)
     
-    print(f'{len(cve_tuple)} unique CVEs returned')    
+    print(f'{len(cve_tuple)} unique CVEs processed')    
     return cve_tuple
     
 
@@ -67,8 +73,72 @@ def update_KEV():
             print('The KEV JSON file has not been downloaded')
 
 
-def rank_CVEs(cve_tuple: tuple):
-    pass
+def get_CVE_data(cve_tuple: tuple):
+    
+    # response
+    
+    for cve in cve_tuple:
+        total_url = NVD_URL+cve      
+        print(f'Processing CVE id: {cve} with URL of: {total_url}')
+        
+        #! Modified CVE
+        # total_url = NVD_URL+'CVE-2017-0170' 
+        #! https://services.nvd.nist.gov/rest/json/cves/2.0?cveId=CVE-2017-0170
+        
+        #! Rejected CVE
+        # total_url = NVD_URL+'CVE-2023-4128' 
+        #! https://services.nvd.nist.gov/rest/json/cves/2.0?cveId=CVE-2023-4128
+        
+        #! No baseSeverity
+        #! https://services.nvd.nist.gov/rest/json/cves/2.0?cveId=CVE-2015-2808
+        # total_url = NVD_URL+'CVE-2015-2808' 
+        
+        baseScore = ""
+        baseSeverity = ""
+        
+        try:
+            response = requests.get(total_url)
+            response_json = response.json()
+        except Exception as e:
+            print(f'CVE Processing Error: {e}')
+        finally:
+            vulnStatus = response_json["vulnerabilities"][0]['cve']['vulnStatus']
+            descriptions_value = response_json["vulnerabilities"][0]['cve']['descriptions'][0]['value']
+
+            if vulnStatus == "Rejected":
+                print(f'{cve} was {vulnStatus} by NVD with a description of: {descriptions_value}')
+                
+            elif vulnStatus != "Rejected":
+                cve_keys = response_json["vulnerabilities"][0]['cve']['metrics'].keys()
+                if "cvssMetricV31" in cve_keys:
+                    # print(f'{cve} has cvssMetricV31')
+                    baseScore = response_json["vulnerabilities"][0]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore']
+                    baseSeverity = response_json["vulnerabilities"][0]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseSeverity']
+                    print(f'{cve:} has a base score of [{baseScore}] and a base severity of !{baseSeverity}!')
+                elif "cvssMetricV30" in cve_keys:
+                    # print(f'{cve} has cvssMetricV30')
+                    baseScore = response_json["vulnerabilities"][0]['cve']['metrics']['cvssMetricV30'][0]['cvssData']['baseScore']
+                    baseSeverity = response_json["vulnerabilities"][0]['cve']['metrics']['cvssMetricV30'][0]['cvssData']['baseSeverity']
+                    print(f'{cve:} has a base score of [{baseScore}] and a base severity of !{baseSeverity}!')
+                elif "cvssMetricV2" in cve_keys:
+                    # print(f'{cve} has cvssMetricV2')
+                    cvssData_keys = response_json["vulnerabilities"][0]['cve']['metrics']['cvssMetricV2'][0]['cvssData'].keys()
+                    if "baseSeverity" in cvssData_keys:
+                        # print('Sev')
+                        baseScore = response_json["vulnerabilities"][0]['cve']['metrics']['cvssMetricV2'][0]['cvssData']['baseScore']
+                        baseSeverity = response_json["vulnerabilities"][0]['cve']['metrics']['cvssMetricV2'][0]['cvssData']['baseSeverity']
+                        print(f'{cve:} has a base score of [{baseScore}] and a base severity of !{baseSeverity}!')
+                    elif "baseSeverity" not in cvssData_keys:
+                        # print('No Sev')
+                        baseScore = response_json["vulnerabilities"][0]['cve']['metrics']['cvssMetricV2'][0]['cvssData']['baseScore']
+                        print(f'{cve:} has a base score of [{baseScore}]')
+                else:
+                    print('Unknown CVSS standard')
+                    
+            # print(f'{cve:} has a base score of [{baseScore}] and a base severity of !{baseSeverity}!')
+            
+        time.sleep(6)
+            
 
     
 
@@ -76,6 +146,5 @@ if __name__ == "__main__":
     print("Welcome to CVE Parse")
     
     update_KEV()
-    CVEs = get_CVEs(path='VulnerabilityReport.xlsx', sheet="CVE", column="CVE")
-    rank_CVEs(CVEs)
-
+    CVEs = extract_CVEs(path='VulnerabilityReport.xlsx', sheet="CVE", column="CVE")
+    get_CVE_data(CVEs)
